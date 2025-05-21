2cfbdac8605eeca3effc4cf81f59c5fb3bf791af
Lukasl32 <Lukasl32@atlas.cz>
2025-05-22 00:22:35 +0200

    Aktualizace layoutu, přidání V2.. pokus o responzivní design

---
+++ Tul.WebApplications.SemesterProject.Web/Pages/Shared/_LayoutV2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tul.WebApplications.SemesterProject.Web.Pages.Shared
+{
+    public class _LayoutV2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
