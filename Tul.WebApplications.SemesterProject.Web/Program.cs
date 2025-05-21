9799f4fa04256c4c7823829ebd568c5c558a83d2
Lukasl32 <Lukasl32@atlas.cz>
2025-05-22 00:21:58 +0200

    Úvodní nastavení, přidání index stránky, přidání předpřipraveného layoutu

--- Tul.WebApplications.SemesterProject.Web/Program.cs
+++ Tul.WebApplications.SemesterProject.Web/Program.cs
@@ -1,5 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseRouting();
+app.MapStaticAssets();
+app.MapRazorPages();
 
 app.Run();
