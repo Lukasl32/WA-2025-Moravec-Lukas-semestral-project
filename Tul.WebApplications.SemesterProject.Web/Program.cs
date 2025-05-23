571d2cdef178199d596b7f195fc867acb0e80132
Lukasl32 <Lukasl32@atlas.cz>
2025-05-23 23:01:17 +0200

    Přidání konfigurace databáze

--- Tul.WebApplications.SemesterProject.Web/Program.cs
+++ Tul.WebApplications.SemesterProject.Web/Program.cs
@@ -1,18 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    internal static string DB { get; private set; }
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var configuration = builder.Configuration;
+        DB = configuration.GetConnectionString("Database:TestDB") ?? throw new InvalidOperationException("Connection string not found.");
+
+        builder.Services.AddRazorPages();
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseRouting();
+        app.MapStaticAssets();
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
-
-app.UseRouting();
-app.MapStaticAssets();
-app.MapRazorPages();
-
-app.Run();
