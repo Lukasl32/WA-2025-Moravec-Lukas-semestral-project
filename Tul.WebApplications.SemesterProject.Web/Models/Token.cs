d924dbfb027cd005f4bbed5a5f6c7c33602c6134
Lukasl32 <Lukasl32@atlas.cz>
2025-05-24 15:03:17 +0200

    Přidání tokenu

---
+++ Tul.WebApplications.SemesterProject.Web/Models/Token.cs
@@ -0,0 +1,9 @@
+﻿using System.Security;
+
+namespace Tul.WebApplications.SemesterProject.Web.Models;
+
+public class Token
+{
+    public Guid Id { get; set; }
+    public SecureString Hash { get; set; }
+}
