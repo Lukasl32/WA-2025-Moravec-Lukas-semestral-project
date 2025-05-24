465ccc217ad0d315bb50e63a56be4c51cbeb0658
Lukasl32 <Lukasl32@atlas.cz>
2025-05-24 14:32:10 +0200

    Přidání modelu uživatele

---
+++ Tul.WebApplications.SemesterProject.Web/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Security;
+
+namespace Tul.WebApplications.SemesterProject.Web.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public SecureString Token { get; set; } = new SecureString(); // Use SecureString for sensitive data like tokens
+
+}
