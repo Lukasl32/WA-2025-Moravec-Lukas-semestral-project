c07e0b75940fbe78459f7087e2f7b0d2495dd58f
Lukasl32 <Lukasl32@atlas.cz>
2025-05-24 14:31:49 +0200

    Přidání login a register stránek

---
+++ Tul.WebApplications.SemesterProject.Web/Pages/User/Login.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tul.WebApplications.SemesterProject.Web.Pages.User
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+        }
+    }
+}
---
+++ Tul.WebApplications.SemesterProject.Web/Pages/User/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tul.WebApplications.SemesterProject.Web.Pages.User
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
