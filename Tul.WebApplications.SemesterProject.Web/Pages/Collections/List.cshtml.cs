2c5f91350547be855c99afff7483d2a7bb4ba128
Lukasl32 <Lukasl32@atlas.cz>
2025-06-02 22:56:40 +0200

    Základ stránek detailu a vypisu kolekcí

---
+++ Tul.WebApplications.SemesterProject.Web/Pages/Collections/List.cshtml.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Tul.WebApplications.SemesterProject.Web.Models;
+
+namespace Tul.WebApplications.SemesterProject.Web.Pages.Collections
+{
+    public class ListModel : PageModel
+    {
+        public List<Collection> GeneratedItems { get; set; } // Replace YourItemModel with your actual model
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var _token = HttpContext.Session.GetString("token");
+            if (string.IsNullOrEmpty(_token))
+                return RedirectToPage("/user/login"); // If the token is not set, redirect to the login page
+
+            var _userId = await Models.UserModel.GetIdByTokenAsync(_token);
+            if (_userId is null)
+                return RedirectToPage("/user/login"); // If the user ID is not found, redirect to the login page
+
+            GeneratedItems = await Collection.GetAllByUserId((Guid)_userId);
+
+            return Page();
+        }
+    }
+}
