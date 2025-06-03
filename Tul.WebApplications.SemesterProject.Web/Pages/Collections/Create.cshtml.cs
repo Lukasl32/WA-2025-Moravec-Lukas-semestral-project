44dfc68cd396c2c493415b837d12685392c14a1c
Lukasl32 <Lukasl32@atlas.cz>
2025-06-03 14:24:37 +0200

    Změna pojmenování modelu o dokončení úprav

--- Tul.WebApplications.SemesterProject.Web/Pages/Collections/Create.cshtml.cs
+++ Tul.WebApplications.SemesterProject.Web/Pages/Collections/Create.cshtml.cs
@@ -5,8 +5,17 @@ namespace Tul.WebApplications.SemesterProject.Web.Pages.Collections
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            var _token = HttpContext.Session.GetString("token");
+            if (string.IsNullOrEmpty(_token))
+                return RedirectToPage("/user/login"); // If the token is not set, redirect to the login page
+
+            var _userId = await Models.UserModel.GetIdByTokenAsync(_token);
+            if (_userId is null)
+                return RedirectToPage("/user/login"); // If the user ID is not found, redirect to the login page
+
+            return Page();
         }
     }
 }
